997f371365cf83b749c6de87803873ab6fea8a9c
Rory Doak <rory.doak@gmail.com>
2023-11-18 07:16:26 +0000

    reconfigure tables and fetch obj to match fields

--- migrations/20231114163033_coin_tables.js
+++ migrations/20231114163033_coin_tables.js
@@ -5,36 +5,47 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable("users", (table) => {
-      table.increments("id").primary();
+      table.string("id").primary();
       table.string("first_name").notNullable();
       table.string("last_name").notNullable();
       table.string("email").notNullable();
+      table.string("known_as");
+      table.string("monzo_user_id");
+      table.string("monzo_token");
     })
     .createTable("accounts", (table) => {
-      table.increments("id").primary();
+      table.string("id").primary();
       table.string("bank_name").notNullable();
       table.integer("account_balance").notNullable();
+      table.integer("savings_balance");
+      table.integer("available_balance");
       table.boolean("is_savings").defaultTo(false);
       table
-        .integer("user_id")
+        .string("user_id")
         .notNullable()
         .unsigned()
         .references("users.id")
         .onUpdate("CASCADE")
         .onDelete("CASCADE");
+      table.string("created_on").notNullable();
+      table.string("type").notNullable();
+      table.string("currency"), notNullable();
+      table.string("country_code").notNullable();
+      table.string("is_closed").notNullable();
+      table.integer("spend_today");
       table.foreign("user_id");
     })
     .createTable("connections", (table) => {
       table.increments("id").primary();
       table
-        .integer("user_id")
+        .string("user_id")
         .notNullable()
         .unsigned()
         .references("users.id")
         .onUpdate("CASCADE")
         .onDelete("CASCADE");
       table
-        .integer("connect_id")
+        .string("connect_id")
         .notNullable()
         .unsigned()
         .references("users.id")
@@ -43,24 +54,28 @@ exports.up = function (knex) {
       table.foreign("user_id");
     })
     .createTable("transactions", (table) => {
-      table.increments().primary();
+      table.string("id").primary();
       table.integer("amount").defaultTo(0);
       table.boolean("is_settled").notNullable();
       table.string("category").notNullable();
       table
-        .integer("user_id")
+        .string("user_id")
         .notNullable()
         .unsigned()
         .references("users.id")
         .onUpdate("CASCADE")
         .onDelete("CASCADE");
       table
-        .integer("account_id")
+        .string("account_id")
         .notNullable()
         .unsigned()
         .references("accounts.id")
         .onUpdate("CASCADE")
         .onDelete("CASCADE");
+      table.string("monzo_account_id").notNullable();
+      table.string("monzo_transaction_id").notNullable();
+      table.integer("savings");
+      table.date("created").notNullable();
 
       table.foreign("user_id");
     });
--- utils/AxiosRequests.js
+++ utils/AxiosRequests.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const token =
-  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6ImR0VlFJNm9wRW5KY2x2WHhmaERvIiwianRpIjoiYWNjdG9rXzAwMDBBYnJrT0VXaXc4amoxVnpDMGgiLCJ0eXAiOiJhdCIsInYiOiI2In0.QcVbity3Y9V1azKCp-ap-uPIRpmmM4nKdjTtCZqha9bOb0wrdDdha9-XZFWK1zyflBIJzc5EDTnHfO9sswOz7w";
+  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6Iitvdm1sSEVlZC9UT1dMM05XcEpsIiwianRpIjoiYWNjdG9rXzAwMDBBYnZJZXNxd2toVnk5QzM4SzIiLCJ0eXAiOiJhdCIsInYiOiI2In0.mmx4VX9VMTuO5kPLNaTnHrPtSCyw74En1H-7VuKJ6_iCaRy7VjAtUrS9zjLSLeCu7ns2tqN1IZ_rrLobyrmu4Q";
 const mainAccountId = "acc_0000A7opetunLsyIaJFg6j";
 const fetchAccountData = async (req, res) => {
   const { data } = await axios.get("https://api.monzo.com/accounts", {
@@ -13,11 +13,13 @@ const fetchAccountData = async (req, res) => {
     .map((account) => {
       return (account = {
         account: account.id,
+        bank_name: "Monzo",
         created: account.created,
         type: account.type,
         currency: account.currency,
         country_code: account.country_code,
         monzo_user_id: account.owners.user_id,
+        is_closed: account.closed,
       });
     });
 
@@ -53,10 +55,8 @@ const fetchTransactionData = async (req, res) => {
       }
     );
 
-    transactionArray = [];
-
-    data.transactions.map((transaction) => {
-      let newObj = {
+    const transactionArray = data.transactions.map((transaction) => {
+      return {
         id: transaction.id,
         account_id: transaction.account_id,
         user_id: transaction.metadata.user_id,
@@ -65,16 +65,15 @@ const fetchTransactionData = async (req, res) => {
         local_amount: transaction.local_amount,
         local_currency: transaction.local_currency,
         created: transaction.created,
+        settled: transaction.settled,
       };
-
-      transactionArray.push(newObj);
     });
 
     res.send(transactionArray);
   } catch (error) {
     console.error(error);
   }
-  return transactionArray;
+  return ["no transactions found"];
 };
 
 module.exports = { fetchAccountData, fetchBalanceData, fetchTransactionData };
