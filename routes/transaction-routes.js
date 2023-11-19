374dcaa035b7766d35d5f3e28c164af586cc7d60
Rory Doak <rory.doak@gmail.com>
2023-11-19 21:31:49 +0000

    add fn to order a weekly spend array

--- routes/transaction-routes.js
+++ routes/transaction-routes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 require("dotenv").config();
 
 router.route("/").get(async (req, res) => {
+  console.log(dayjs("2021-11-18").week());
   const transactionArray = await fetchWeeklySpend();
 
   const transactionsByWeekArray = transactionArray.map(
@@ -17,7 +18,76 @@ router.route("/").get(async (req, res) => {
         week: dayjs(transaction.created).week(),
       })
   );
+
+  let newArray = [];
+
+  transactionsByWeekArray.map((transaction) => {
+    let foundIndex = newArray.findIndex(
+      (obj) => obj.name === transaction.first_name
+    );
+    if (
+      transaction.category === "transfers" ||
+      transaction.category === "income" ||
+      transaction.category === "savings"
+    ) {
+      return;
+    }
+    if (foundIndex === -1) {
+      // If you can't find the name
+      newArray = [
+        ...newArray,
+        {
+          name: transaction.first_name,
+          data: [{ week: transaction.week, amount: transaction.amount }],
+        },
+      ];
+      return;
+    }
+    let weekIndex = newArray[foundIndex].data.findIndex(
+      (obj) => obj.week === transaction.week
+    );
+
+    // If you can find the name and the week you are looking for is there
+    if (foundIndex !== -1 && weekIndex !== undefined && weekIndex !== -1) {
+      // we need to change the amount on the amount: key
+      let newAmount = 0;
+      newAmount =
+        newArray[foundIndex].data[weekIndex].amount + transaction.amount;
+      newArray[foundIndex].data[weekIndex].amount = newAmount;
+      return;
+    }
+    // If you can find the name, but the week you are looking for is not there
+    if (foundIndex !== -1 && weekIndex === -1) {
+      newArray[foundIndex].data = [
+        ...newArray[foundIndex].data,
+        { week: transaction.week, amount: transaction.amount },
+      ];
+      return;
+    }
+  });
+  const roryUnitChange = newArray[0].data.map((week) => {
+    return (newObj = { ...week, amount: week.amount / 100 });
+  });
+  console.log(roryUnitChange);
+  newArray[0].data = roryUnitChange;
   res.status(200).send(transactionsByWeekArray);
+  return newArray;
 });
 
 module.exports = router;
+
+// [
+//   {
+//     name: "Rory",
+//     data: [
+//       {
+//         week: 47,
+//         amount: 2344,
+//       },
+//       {
+//         week: 48,
+//         amount: 2344,
+//       },
+//     ],
+//   },
+// ];
